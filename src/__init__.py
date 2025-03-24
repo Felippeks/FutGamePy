c8f404fbd492e566dbebd7fe0a917e79fdb31920
Felippe Kulkamp Sant Ana <felippeks@gmail.com>
2025-03-24 13:30:17 -0300

    alteração extrutural

--- src/__init__.py
+++ src/__init__.py
@@ -1,10 +1,1 @@
-from .config import Config
-from .asset_loader import AssetLoader
-from .game_state import GameState
-from .sound_manager import SoundManager
-from .ball import Ball
-from .paddle import Paddle
-from .physics_engine import PhysicsEngine
-from .ui_manager import UIManager
-from .input_handler import InputHandler
-from .game import Game
+__version__ = "1.0.0"
